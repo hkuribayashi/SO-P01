95ae94f68ea846bb93ee017a56f85f99e0cf4b1a
hkuribayashi <hkuribayashi@gmail.com>
2023-09-20 20:43:38 -0300

    Commit Inicial

---
+++ exemplo02.c
@@ -0,0 +1,35 @@
+// C program to implement
+// the above approach
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	FILE* ptr;
+	char ch;
+
+	// Abertura de um arquivo de modo de leitura
+	ptr = fopen("texto.txt", "r");
+
+	if (NULL == ptr) {
+		printf("arquivo nao pode ser aberto \n");
+	}else{
+
+		printf("Conteudo do Arquivo: \n");
+
+		// Imprimindo o que esta escrito no arquivo,
+		// character by character using loop.
+		do {
+			ch = fgetc(ptr);
+			printf("%c", ch);
+
+			// Checando se o caracter é EOF.
+			// Se EOF pare a leitura.
+		} while (ch != EOF);
+	}
+	// Fechando o Arquivo
+	fclose(ptr);
+	return 0;
+}
+
